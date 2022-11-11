64c01419b917878e3132cb0ce04ee51f864d1d7d
Lucas Paris <lucas.paris@ciapetro.com.br>
2022-11-11 08:35:49 -0300

    First commit

---
+++ dist/src/OrderItem.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class OrderItem {
+    constructor(idItem, price, quantity) {
+        this.idItem = idItem;
+        this.price = price;
+        this.quantity = quantity;
+    }
+}
+exports.default = OrderItem;
